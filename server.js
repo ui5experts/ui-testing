3732e26c2f4abf09f4af586e4605c1107e51912c
Christian Schuff <christian.schuff@gmail.com>
2017-09-05 19:51:46 +0200

    adds server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = require('express')();
+const proxyMiddleware = require('http-proxy-middleware');
+
+// statically serve webapp folder, use test.html as index
+app.use(express.static('webapp', { index: 'test.html' }));
+
+// proxy resources and test-resources from CDN, add specific version if needed
+app.use(proxyMiddleware('/resources/**', {
+  target: 'https://sapui5.hana.ondemand.com/',
+  changeOrigin: true
+}));
+app.use(proxyMiddleware('/test-resources/**', {
+  target: 'https://sapui5.hana.ondemand.com/',
+  changeOrigin: true
+}));
+
+// start server
+app.listen(3000, function () {
+  console.log('Express server listening on port 3000!');
+});
